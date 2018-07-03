de89f9be5171600a40b4745b725b57fe313075b7
107295472 <yintingji@gmail.com>
2018-07-03 11:12:28 +0800

    init

---
+++ SqlFunc/Utils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace SqlFunc
+{
+   public class Utils
+    {
+        [Microsoft.SqlServer.Server.SqlFunction]
+        public static string UrlEncode(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            byte[] byStr = System.Text.Encoding.UTF8.GetBytes(str); //默认是System.Text.Encoding.Default.GetBytes(str)
+            for (int i = 0; i < byStr.Length; i++)
+            {
+                sb.Append(@"%" + Convert.ToString(byStr[i], 16));
+            }
+
+            return (sb.ToString());
+        }
+    }
+}
